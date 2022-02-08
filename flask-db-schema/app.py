891cff790601e8bcad536bb5b49df69cd26591ab
LukeBenson <luke_benson@outlook.com>
2022-02-08 16:30:05 +0000

    changing database URI for local MySQL instances

--- flask-db-schema/app.py
+++ flask-db-schema/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# For the config to work here, I have a local MySQL database running and have already created a flaskdb database in MySQL.
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:MYPASSWORD@localhost:3306/flaskdb"
 
 db = SQLAlchemy(app)
 
